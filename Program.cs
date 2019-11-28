3368e340fda2b8b14eaf755111272339ef8e7111
kierepkae <kierepkaedyta@gmail.com>
2019-11-28 19:15:14 +0000

    use Add in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System;
 namespace MethodsTraining {
   class Program {
     static void Main (string[] args) {
-
+      var calculator = new Calculator ();
+      var result1 = calculator.Add (new int[] { 1, 2, 3, 4, });
+      var result2 = calculator.Add ({ 1, 2, 3, 4, });
     };
   }
 }
