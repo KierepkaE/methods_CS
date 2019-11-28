30ea80065f8b532cca7bff72e78d63c2551fc68a
kierepkae <kierepkaedyta@gmail.com>
2019-11-28 20:48:44 +0000

    create Point instance

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace MethodsTraining {
       var calculator = new Calculator ();
       var result1 = calculator.Add (new int[] { 1, 2, 3, 4, });
       var result2 = calculator.Add ({ 1, 2, 3, 4, });
+      var point = new Point (10, 20);
+      point.Move (new Point (34, 21));
+
+      point.Move (100, 200);
+      Console.WriteLine ("Point is: {0},{1}", point.X, point.Y);
     };
   }
 }
